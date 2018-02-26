95297d15232b626c2279a661bec7ac3be6fac5b7
javatlacati <scherzo_16@hotmail.com>
2018-02-25 21:43:30 -0600

    added performance testing for lists

---
+++ public/tests/list/PerformanceForLists.ts
@@ -0,0 +1,36 @@
+import * as Benchmark from 'benchmark';
+import {lists} from "../../scripts/list/lists";
+
+let instancesToBenchmark = [
+    new lists.linkedlists.singlelinkedlists.iterative.SingleLinkedList<String>(),
+    new lists.linkedlists.singlelinkedlists.iterative.TailedSingleLinkedList<String>()
+    /*,
+    lists.linkedlists.singlelinkedlists.recursive.SingleLinkedList.class,
+*/
+];
+
+it('benchmarks', () => {
+    instancesToBenchmark.forEach((instanceTobenchmark) => {
+            let suite = new Benchmark.Suite;
+            suite
+                .add('add benchmark for instance ' + instanceTobenchmark.constructor,
+                    () => instanceTobenchmark.add("hola" + Math.random())
+                )
+                .add('emptiness benchmark for instance ' + instanceTobenchmark.constructor,
+                    () => instanceTobenchmark.isEmpty()
+                )
+                .add('size benchmark for instance ' + instanceTobenchmark.constructor,
+                    () => instanceTobenchmark.size()
+                )
+                .add('contains benchmark for instance ' + instanceTobenchmark.constructor,
+                    () => instanceTobenchmark.contains("hola" + Math.random())
+                )
+                .on('cycle', function (event: any) {
+                        // add listeners
+                        console.log(String(event.target));
+                    }
+                ).run({'async': true})
+        }
+    );
+
+});
